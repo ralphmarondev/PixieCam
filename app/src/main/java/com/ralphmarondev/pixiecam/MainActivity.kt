a913fa3a73537b0bb0d8d022e3117f83ae61ebba
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-02-25 15:36:23 +0800

    implemented switching camera preview [front and back]

--- app/src/main/java/com/ralphmarondev/pixiecam/MainActivity.kt
+++ app/src/main/java/com/ralphmarondev/pixiecam/MainActivity.kt
@@ -6,13 +6,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.camera.core.CameraSelector
 import androidx.camera.view.CameraController
 import androidx.camera.view.LifecycleCameraController
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Cameraswitch
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -57,10 +63,28 @@ class MainActivity : ComponentActivity() {
                             .padding(innerPadding)
                     ) {
                         CameraPreview(
-                            controller =controller,
+                            controller = controller,
                             modifier = Modifier
                                 .fillMaxSize()
                         )
+
+                        IconButton(
+                            onClick = {
+                                controller.cameraSelector =
+                                    if (controller.cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) {
+                                        CameraSelector.DEFAULT_FRONT_CAMERA
+                                    } else {
+                                        CameraSelector.DEFAULT_BACK_CAMERA
+                                    }
+                            },
+                            modifier = Modifier
+                                .offset(x = 16.dp, y = 32.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Outlined.Cameraswitch,
+                                contentDescription = "Switch camera"
+                            )
+                        }
                     }
                 }
             }
